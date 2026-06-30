5847eac40464af51660e41c1f6dd3f12cb7a1eff
fardinkhanfahmi <fardinkhanfahmi@gmail.com>
2026-07-01 02:46:23 +0600

    Add files via upload

---
+++ vector_loop.rs
@@ -0,0 +1,6 @@
+pub fn run() {
+    let v = vec![10, 20, 30];
+    for x in &v {
+        println!("Vector: {}", x);
+    }
+}
---
+++ while_loop.rs
@@ -0,0 +1,7 @@
+pub fn run() {
+    let mut i = 1;
+    while i <= 5 {
+        println!("{}", i);
+        i += 1;
+    }
+}
